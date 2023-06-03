4bdcf25cdd98b62f1a3c51759d421888afa4b7c7
gunyakov <oleg_goodzon@mail.ru>
2023-06-03 13:59:13 +0300

    Compile errors resolve.

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ function getGlobalObject() {
 	if (typeof globalThis !== 'undefined') { return globalThis; }
 	if (typeof self !== 'undefined') { return self; }
 	if (typeof window !== 'undefined') { return window; }
-	if (typeof global !== 'undefined') { return global; }
+	//if (typeof global !== 'undefined') { return global; }
 
 	throw new Error('Unable to locate global object.');
 }
